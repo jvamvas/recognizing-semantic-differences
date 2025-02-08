f483319e9b2dc755160e87848d2cb60a920cd233
Jannis Vamvas <jvamvas@users.noreply.github.com>
2025-02-08 18:05:09 +0000

    Add modernbert evaluation script

---
+++ experiments/scripts/create_test_table_modernbert.py
@@ -0,0 +1,42 @@
+from collections import OrderedDict
+
+from transformers import pipeline
+
+from experiments.benchmark import DifferenceRecognitionResult
+from experiments.utils import load_summary_benchmarks
+from recognizers import DiffAlign
+
+
+benchmarks = load_summary_benchmarks("test")[0]
+device = 0
+
+recognizers = []
+recognizers.append(DiffAlign(
+    pipeline=pipeline(
+        model="answerdotai/ModernBERT-base",
+        task="feature-extraction",
+    ),
+    batch_size=4,
+))
+
+results = OrderedDict()
+for i, recognizer in enumerate(recognizers):
+    print(recognizer)
+    recognizer.pipeline.device = device
+    recognizer.device = device
+    recognizer.pipeline.model = recognizer.pipeline.model.to(device)
+    recognizer_results = []
+    for benchmark in benchmarks:
+        print(benchmark)
+        result = benchmark.evaluate(recognizer)
+        print(result)
+        recognizer_results.append(result)
+    # Average last six results (cross-lingual)
+    recognizer_results, cross_lingual_results = recognizer_results[:-6], recognizer_results[-6:]
+    cross_lingual_mean = sum([result.spearman for result in cross_lingual_results]) / len(cross_lingual_results)
+    recognizer_results.append(DifferenceRecognitionResult(spearman=cross_lingual_mean))
+    results[str(recognizer)] = recognizer_results
+    recognizers[i] = None
+    del recognizer
+
+print(results)
